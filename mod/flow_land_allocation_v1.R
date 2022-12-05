f52594d4a7ff60a64d1699a588034315e315945a
Oliver Perkins <oliverperkins@live.co.uk>
2022-12-05 13:17:03 +0000

    improved land allocation mechanism

--- mod/flow_land_allocation_v1.R
+++ mod/flow_land_allocation_v1.R
@@ -6,6 +6,8 @@
 
 ###################################################################################
 
+#calculates income pressure on land use allocation
+
 income_pressure <- function(sf) {
   
   sf <- lapply(sf, function(x) {
@@ -14,7 +16,7 @@ income_pressure <- function(sf) {
     
     lapply(x, function(y, ip = income) {
       
-      y@s_parameters$s_income_pressure <- y@s_parameters$s_income / mean(ip, na.rm= T)
+      y@s_parameters$s_income_pressure <- 1 - (y@s_parameters$s_income / mean(ip, na.rm= T))
       
       y
     })
@@ -27,22 +29,51 @@ income_pressure <- function(sf) {
 }
 
 
-land_pressure <- function(s_forest, 
-                          s_cereals,
-                          s_veg,
-                          s_dairy, 
-                          s_rg, p_baseline_land_trans = 14.771*1000) {
-
-  s.fam <- stock_by_family(mod = mod)
-  s.fam <- income_pressure(s.fam)
-  s.fam <- lapply(unlist(s.fam), function(x) {
-    
-            x@s_parameters$s_income_pressure <- (1-x@s_parameters$s_income_pressure) * p_baseline_land_trans
-            x
-    
-                })
+lc_stock <- function(sf) {
   
+  #helper to find supply of less competitive land system for change
+  
+  land_stock <- lapply(sf, function(x) {
     
-    return(unlist(s.fam))
+                  which.min(unlist(lapply(x, function(y) {
+    
+                    y@s_parameters$s_income
+    
+                      })))
+  
+                        })
+
+ 
+  
+   
+}
+  
+### converts income pressure to a hectares change
+
+land_pressure <- function(p_land_trans = 0.0035, ...) {
+
+  l <- list(...)
+
+  s.fam   <- stock_by_family(l)
+  s.fam   <- income_pressure(s.fam)
+  s.small <- lc_stock(s.fam) 
+  
+  for(i in 1:length(s.fam)) {
+    
+    delta_area <- s.fam[[i]][[s.small[[i]]]]@s_parameters$s_area
+    
+    for(j in 1:length(s.fam[[i]])) {
+    
+    s.fam[[i]][[j]]@s_parameters$s_income_pressure <- (s.fam[[i]][[j]]@s_parameters$s_income_pressure) * 
+                                                          p_land_trans * delta_area
+    
+      }
   
   }
+
+    return(unname(unlist(s.fam)))
+  
+}
+
+
+
