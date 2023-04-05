a20b1173837d65c209208a5f52a7f56cb252752b
Oliver Perkins <oliverperkins@live.co.uk>
2023-04-05 11:35:14 +0100

    Readme first draft

--- R/Flow.R
+++ R/Flow.R
@@ -5,128 +5,167 @@
 
 ###############################################################################
 
-#' @export
-flow <- setClass('flow', 
-          slots=list(f_name="character", f_id="character",
-           f_from_stocks = "character", f_to_stocks = "character", 
-            f_function = "character", f_values = "list"))
+#'Initialise a model stock
+#'@param f_name A character; if using standard taxonomy starting with 'f_'
+#'@param f_id A character giving a unique object identifier
+#'@param f_from_stocks A character naming stocks and inputs that are upstream in the model structure
+#'@param f_to_stocks A character naming stocks that are downstream in the model structure
+#'@param f_function A character naming the function to be used to run the flow; this can be loaded from a package or defined in the global environment
+#'@param f_values An empty list to be filled by flow function inputs and outputs
+#'@returns A rhapsody flow
+#'@examples
+#'f_deforest <- flow(f_name = 'f_deforest', f_id = 'f1', f_from_stocks = c('s_soybean, s_forest'),
+#'              f_to_stocks = c('s_soybean, s_forest'), f_function = 'deforest')
 
+#' @export
+flow <- setClass('flow',
+          slots=list(f_name="character", f_id="character",
+           f_from_stocks = "character", f_to_stocks = "character",
+            f_function = "character", f_values = "list"))
 
 ### helpers
 #' @export
 setGeneric("check_flow_function", function(x) standardGeneric("check_flow_function"))
 setMethod("check_flow_function", "flow", function(x){typeof(get(x@f_function)) == 'closure'})
-    
+
 setGeneric("parse_flow_args", function(x, taxon) standardGeneric("parse_flow_args"))
 setMethod("parse_flow_args", "flow", function(x, taxon) {
-  
+
   for(f in 1:length(x@f_values$inputs)) {
-    
+
     if(taxon[f] == 'inputs') {
-      
-      x@f_values$inputs[[f]] <- list(current = x@f_values$input[[f]]@i_current_val, 
+
+      x@f_values$inputs[[f]] <- list(current = x@f_values$input[[f]]@i_current_val,
                                     data = x@f_values$input[[f]]@i_data)
-      
+
     } else if(taxon[f] == 'stocks') {
-      
+
       x@f_values$inputs[[f]] <- x@f_values$input[[f]]@s_parameters
-      
+
     }
-    
+
   }
-  
+
   x
-  
+
 })
 
 
 ### getters
+#'get values of flow arguments from model object
+#'@param x A rhapsody flow object
+#'@param mod A rhapsody model object
+#'@param parse_args A logical; should only the data elements of relevant objects be returned?
+#'@returns A rhapsody flow
+#'@examples
+#'f_deforest        <- get_flow_args(f_deforest, sd.mod, parse_args = F)
+#'sd.mod@flows[[1]] <- get_flow_args(sd.mod@flows[[1]], sd.mod)
+
 setGeneric("get_flow_args", function(x, mod, parse_args) standardGeneric("get_flow_args"))
 setMethod("get_flow_args", "flow", function(x, mod, parse_args = T) {
-  
+
   f_args            <- names(formals(get(x@f_function)))
   tax               <- arg_parser(mod, f_args)
-  
+
   result <- sapply(1:length(tax), function(z) {
-    
+
     if(tax[z] == 'inputs') {
-      
+
       r <- mod@inputs[[which(unlist(lapply(mod@inputs, function(i) (i@i_name))) == f_args[z])]]
-      
+
     } else if(tax[z] == 'parameters') {
-      
+
       r <- mod@parameters[[f_args[z]]]
-      
-    } else if(tax[z] == 'stocks') { 
+
+    } else if(tax[z] == 'stocks') {
 
       r   <- mod@stocks[[which(unlist(lapply(mod@stocks, function(s) (s@s_name))) == f_args[z])]]
 
     }
-    
+
     r        <- list(r)
     names(r) <- f_args[z]
     r
-    
+
   })
-  
+
   x@f_values$inputs <- result
-  
+
   if(parse_args == T) {
-    
+
     x <- parse_flow_args(x, tax)
-    
+
   }
-  
+
   x
 
 })
+
+
+
+### run the flow
+#'runs flow
+#'@param x A rhapsody flow
+#'@returns A rhapsody flow
+#'@examples
+#'f_deforest <- run_flow(f_deforest)
 
 ### run flow
 setGeneric("run_flow", function(x, ...) standardGeneric("run_flow"))
 setMethod("run_flow", "flow", function(x, ...) {
 
   x@f_values$outputs <- do.call(x@f_function, x@f_values$inputs)
-  
+
   x
-  
+
 })
-      
+
+
+
+### pushes results back to the model
+#'pushes flow outputs back to the model
+#'@param x A rhapsody flow
+#'@param mod A rhapsody model
+#'@returns A rhapsody model object
+#'@examples
+#'sd.mod <- set_flow_output(sd.mod@flows[[1]], sd.mod)
+
 ### setters
 setGeneric("set_flow_output", function(x, mod) standardGeneric("set_flow_output"))
 setMethod("set_flow_output", "flow", function(x, mod) {
-  
+
   f_outs <- names(x@f_values$outputs)
   tax    <- arg_parser(mod, f_outs)
-  
+
   for(z in 1:length(tax)) {
-    
+
     if(tax[z] == 'stocks') {
-      
+
       for(out in 1:length(x@f_values$outputs[[z]])) {
-      
-      mod@stocks[[which(unlist(lapply(mod@stocks, 
+
+      mod@stocks[[which(unlist(lapply(mod@stocks,
           function(s) (s@s_name))) == f_outs[z])]]@s_parameters[names(
             x@f_values$outputs[[z]][out])] <- x@f_values$outputs[[z]][[out]]
-      
-          } 
-      
+
+          }
+
     } else if(tax[z] == 'outputs') {
-          
+
       for(out in 1:length(x@f_values$outputs[[z]])) {
-        
-        mod@outputs[[which(unlist(lapply(mod@outputs, 
+
+        mod@outputs[[which(unlist(lapply(mod@outputs,
           function(o) (o@o_name))) == f_outs[z])]]@o_current_val[names(
             x@f_values$outputs[[z]][out])] <- x@f_values$outputs[[z]][[out]]
-        
-      } 
-      
+
+      }
+
         }
-      
+
       }
 
   mod
-  
-  
+
+
 })
 
 ###########################################################################
@@ -143,115 +182,132 @@ setClass('family_flow', contains = 'flow')
 ## getter for family flow
 ##########################
 
+#'gets argument values for a family flow object
+#'@param x A rhapsody family flow
+#'@param mod A rhapsody model
+#'@param parse_args A logical - should only data components of model objects be returned
+#'@returns A rhapsody family flow object
+#'@examples
+#'sd.mod@flows[[1]] <- get_ff_args(sd.mod@flows[[1]], sd.mod)
+
+
 setGeneric("get_ff_args", function(x, mod, parse_args) standardGeneric("get_ff_args"))
 setMethod("get_ff_args", "family_flow", function(x, mod, parse_args = F) {
-  
+
   f_args            <- names(formals(get(x@f_function)))
   tax               <- arg_parser(mod, f_args)
-  
+
   result <- sapply(1:length(tax), function(z) {
-    
+
     if(tax[z] == 'inputs') {
-      
+
       r <- mod@inputs[[which(unlist(lapply(mod@inputs, function(i) (i@i_name))) == f_args[z])]]
       r        <- list(r)
       names(r) <- f_args[z]
-      
+
     } else if(tax[z] == 'parameters') {
-      
+
       r <- mod@parameters[[f_args[z]]]
       r        <- list(r)
       names(r) <- f_args[z]
-      
-    } else if(tax[z] == 'family_flows') { 
-      
+
+    } else if(tax[z] == 'family_flows') {
+
       r   <- list()
 
       for(s in 1:length(x@f_from_stocks)) {
-      
+
       r[[s]]        <- mod@stocks[which(unlist(lapply(mod@stocks, function(s) (s@s_family))) == x@f_from_stocks[s])]
       names(r)[[s]] <- x@f_from_stocks[s]
-      
+
     }
-    
+
     r        <- list(r)
     names(r) <- 'ff_'
-    
+
     }
-    
+
     r
-    
+
   })
-  
+
   x@f_values$inputs <- result
-  
+
   if(parse_args == T) {
-    
+
     x <- parse_flow_args(x, tax)
-    
+
   }
-  
+
   x
-  
+
 })
 
 ############################
 ### setter for family flow
 ############################
 
+#'sets flow outputs for a family flow object
+#'@param x A rhapsody family flow
+#'@param mod A rhapsody model
+#'@returns A rhapsody model object
+#'@examples
+#'sd.mod <- set_ff_output(sd.mod@flows[[1]], sd.mod)
+
+
 setGeneric("set_ff_output", function(x, mod) standardGeneric("set_ff_output"))
 setMethod("set_ff_output", "family_flow", function(x, mod) {
-  
+
   f_outs <- names(x@f_values$outputs)
   tax    <- arg_parser(mod, f_outs)
-  
+
   for(z in 1:length(tax)) {
-    
+
     if(tax[z] == 'stocks') {
-      
+
       for(out in 1:length(x@f_values$outputs[[z]])) {
-        
-        mod@stocks[[which(unlist(lapply(mod@stocks, 
+
+        mod@stocks[[which(unlist(lapply(mod@stocks,
                                         function(s) (s@s_name))) == f_outs[z])]]@s_parameters[names(
                                           x@f_values$outputs[[z]][out])] <- x@f_values$outputs[[z]][[out]]
-        
-      } 
-      
+
+      }
+
     } else if(tax[z] == 'outputs') {
-      
+
       for(out in 1:length(x@f_values$outputs[[z]])) {
-        
-        mod@outputs[[which(unlist(lapply(mod@outputs, 
+
+        mod@outputs[[which(unlist(lapply(mod@outputs,
             function(o) (o@o_name))) == f_outs[z])]]@o_current_val[names(
               x@f_values$outputs[[z]][out])] <- x@f_values$outputs[[z]][[out]]
-        
-      } 
-      
-      ### family flow outputs should 
-      
+
+      }
+
+      ### family flow outputs should
+
     } else if(tax[z] == 'family_flows') {
-      
+
       fields <- x@f_values$outputs[[z]][[2]]
-      
+
        for(s in x@f_values$outputs[[z]][[1]]) {
-         
+
          sn <- which(unlist(lapply(mod@stocks, function(y) {y@s_name})) == s@s_name)
-         
+
          for(f in fields) {
-           
+
            mod@stocks[[sn]]@s_parameters[f] <- s@s_parameters[f]
-           
+
          }
-         
+
        }
-      
+
     }
-    
+
   }
-  
+
   mod
-  
-  
+
+
 })
 
 
--- R/Stock.R
+++ R/Stock.R
@@ -6,23 +6,40 @@
 ###############################################################################
 
 #' @export
-.stock <- setClass('stock', 
-          slots=list(s_name="character", 
+.stock <- setClass('stock',
+          slots=list(s_name="character",
            s_id="character", s_family = "character",
-            s_flows_out = "character", s_flows_in = "character", 
+            s_flows_out = "character", s_flows_in = "character",
              s_internal_pars = "character", s_parameters = "list"))
 
-init_stock <- function(name, id, family, flows_out, flows_in, 
+
+### Initialise a stock
+#'Initialise a model stock
+#'@param name A character string; if using standard taxonomy starting with 's_'
+#'@param id A character string giving a unique object identifier
+#'@param family A character string assigning the stock to a family of stocks
+#'@param flows_out A character naming stocks and outputs that are downstream in the model structure
+#'@param flows_in A character naming stocks and inputs that are uptream in the model structure
+#'@param internal_pars A character vector naming the stocks internal parameters
+#'@param parameters An empty list to be filled by model intial parameters
+#'@returns A rhapsody stock object
+#'@examples
+#'s_soybeans <- init_stock(s_name = 's_soybean',
+#'     s_family = 'cropland', s_id = 's1',
+#'          s_flow_out = 's_soybean', s_flow_in = 'i_soybean_demand',
+#'            s_parameters = c('area', 'yield', 'price'), s_internal_pars = '')
+
+init_stock <- function(name, id, family, flows_out, flows_in,
                        internal_pars, parameters = list()) {
-  
-  s <- .stock(s_name = name, s_id = id, s_family = family, s_flows_out = flows_out, 
+
+  s <- .stock(s_name = name, s_id = id, s_family = family, s_flows_out = flows_out,
               s_internal_pars = internal_pars)
 
   ## deal with case of no internal parameters
   s@s_internal_pars <- ifelse(identical(s@s_internal_pars, character(0)), '', s@s_internal_pars)
-    
+
   s
-  
+
 }
 
 
@@ -32,84 +49,107 @@ init_stock <- function(name, id, family, flows_out, flows_in,
 
 ###############################################################################
 
+#'Load stock internal parameter data set
+#'@param x A rhapsody stock
+#'@param path Either NA, which uses defaults to 'stockname.csv', or a file path giving the location of the stock internal data
+#'@returns A rhapsody stock object
+#'@examples
+#'s_soybeans <- load_stock_data(s_soybeans, NA)
+
 #' @export
 setGeneric("load_stock_data", function(x, path) standardGeneric("load_stock_data"))
 setMethod("load_stock_data", "stock", function(x, path) {
-  
+
   if(x@s_internal_pars[1] != "") {
-  
+
     for(p in 1:length(x@s_internal_pars)) {
-      
+
       x@s_parameters[x@s_internal_pars[p]] <- 0
-      
+
     }
-    
+
     if(is.na(path)) {
-    
+
       path <- paste0(x@s_name, '.csv')
 
     }
-  
+
     x@s_parameters$internal <- read.csv(path)
-    
+
   }
-    
+
   x
-  
+
 })
+
+
+
+#'Load stock parameters: these are those that are dynamically updated by other model objects
+#'@param x A rhapsody stock
+#'@param isv A dataframe, typically that passed to the initial_parameters argument of init_model
+#'@returns A rhapsody stock object
+#'@examples
+#'s_soybeans <- init_pars(s_soybeans, start_vals)
 
 #' @export
 setGeneric('init_pars', function(x, isv) standardGeneric('init_pars'))
 setMethod('init_pars', 'stock', function(x, isv) {
-  
+
   r <- which(isv[, 1] == x@s_name)
-  
+
   for(i in r) {
-    
+
     x@s_parameters[isv[i, 2]] <- isv[i,3]
-    
+
   }
-  
+
   x
-  
+
 })
 
 
 
+#'Load stock parameters: these are those that are dynamically updated by other model objects
+#'@param x A rhapsody stock object
+#'@param mod A rhapsody model object
+#'@returns A rhapsody stock object
+#'@examples
+#'s_soybeans <- update_internal_par(s_soybeans, sd.mod)
+
 #' @export
 setGeneric('update_internal_par', function(x, mod) standardGeneric("update_internal_par"))
 setMethod('update_internal_par', 'stock', function(x, mod){
-  
+
   if(!any(grepl('internal', names(x@s_parameters))) | nchar(x@s_internal_pars[1]) == 0) {
-    
+
     warning('Trying to update stock with no internal parameters')
     return(x)
-    
+
   }
-  
+
   step <- mod@run$step
   df   <- x@s_parameters$internal
 
   for(i in 1:length(x@s_internal_pars)) {
-    
+
       if(length(dim(df)) == 2) {
-    
+
         x@s_parameters[x@s_internal_pars[i]] <- df[df[, 1] == step, which(names(df) == x@s_internal_pars[i])]
-    
+
   } else if(length(dim(df)) == 3) {
-    
+
     x@s_parameters[x@s_internal_pars[i]] <- df[[which(names(df)) == x@s_internal_pars[i]]][, , step]
-    
+
   } else {
-    
+
     stop('Input data must be either a 2-d or 3-d array or array-like')
-    
+
       }
-  
+
     }
 
   x
-  
+
 })
 
 
@@ -123,22 +163,22 @@ setClass('spatial_stock', contains = 'stock')
 
 setGeneric("load_spatial_stock", function(x, path) standardGeneric("load_spatial_stock"))
 setMethod("load_spatial_stock", "stock", function(x, path) {
-  
-  
+
+
   if(is.na(path)) {
-    
+
     path <- paste0(x@s_name, '.nc')
-    
+
   }
-  
+
   x@s_parameters$internal           <- raster::brick(path)[]
   col                               <- ncol(x@s_parameters$internal)
   r                                 <- nrow(x@s_parameters$internal)
-  x@s_parameters$internal           <- cbind(rep(1:col, each = r, array(x@i_data, 
+  x@s_parameters$internal           <- cbind(rep(1:col, each = r, array(x@i_data,
                                          dim = col*nrow(x@i_data))))
-  
+
   x
-  
+
 })
 
 
@@ -150,7 +190,7 @@ setMethod("load_spatial_stock", "stock", function(x, path) {
 
 #' @export
 setClass('family_stock', contains = 'stock',
-  slots=list(s_family_name="character", s_family_id="character", 
+  slots=list(s_family_name="character", s_family_id="character",
     s_family_members = "character",
      s_family_flows_in = "character", s_family_flows_out = "character"))
 
